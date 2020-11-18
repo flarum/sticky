284dfc1644080279a716f931cba2abdb735da5d1
SychO9 <sychocouldy@gmail.com>
2020-11-18 13:43:22 +0100

    Use new Post extender

--- extend.php
+++ extend.php
@@ -10,12 +10,16 @@
 use Flarum\Api\Serializer\DiscussionSerializer;
 use Flarum\Extend;
 use Flarum\Sticky\Listener;
+use Flarum\Sticky\Post\DiscussionStickiedPost;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/less/forum.less'),
+
+    (new Extend\Post())
+        ->type(DiscussionStickiedPost::class),
 
     (new Extend\ApiSerializer(DiscussionSerializer::class))
         ->attribute('isSticky', function (array $attributes, $discussion) {
